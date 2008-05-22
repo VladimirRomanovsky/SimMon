a3304d7af5e06bae226567cbc8c39dd1e7d1f004
roma <roma@e2913486-074d-0410-a0bf-e5b2e5bbeee3>
2008-05-22 16:44:24 +0000

    Merge SeMuOnOff
    git-svn-id: svn://neomap.ihep.su/data2/roma/svn/repos/SimMon/trunk@30 e2913486-074d-0410-a0bf-e5b2e5bbeee3

--- SeMuOnOff.py
+++ SeMuOnOff.py
@@ -1,6 +1,6 @@
 from ROOT import TH1F, TH2F
 
-class ViewMU:
+class SeMuOnOff:
 
 	def __init__(self,rootfile):
 		
@@ -13,18 +13,18 @@ class ViewMU:
 		self.hc2 = []
 		self.hc3 = []
 		for i in range(4):
-			self.h.append(TH1F( 'h%i'%i, 'h%i'%i,200, 0, 2000 ))
-			self.hc1.append(TH1F( 'hc1_%i'%i, 'hc1_%i'%i,200, 0, 2000 ))
-			self.hc2.append(TH1F( 'hc2_%i'%i, 'hc2_%i'%i,200, 0, 2000 ))
-			self.hc3.append(TH1F( 'hc3_%i'%i, 'hc3_%i'%i,200, 0, 2000 ))
+			self.h.append(TH1F( 'h%i'%i, 'h%i'%i,200, 0, 4000 ))
+			self.hc1.append(TH1F( 'hc1_%i'%i, 'hc1_%i'%i,200, 0, 4000 ))
+			self.hc2.append(TH1F( 'hc2_%i'%i, 'hc2_%i'%i,200, 0, 4000 ))
+			self.hc3.append(TH1F( 'hc3_%i'%i, 'hc3_%i'%i,200, 0, 4000 ))
 		
 		
-		self.h01 = TH2F( 'h_01', 'h_01', 100, 0, 2000, 100, 0, 2000 )
-		self.h02 = TH2F( 'h_02', 'h_02', 100, 0, 2000, 100, 0, 2000 )
-		self.h03 = TH2F( 'h_03', 'h_03', 100, 0, 2000, 100, 0, 2000 )
-		self.h12 = TH2F( 'h_12', 'h_12', 100, 0, 2000, 100, 0, 2000 )
-		self.h13 = TH2F( 'h_13', 'h_13', 100, 0, 2000, 100, 0, 2000 )
-		self.h23 = TH2F( 'h_23', 'h_23', 100, 0, 2000, 100, 0, 2000 )
+		self.h01 = TH2F( 'h_01', 'h_01', 100, 0, 4000, 100, 0, 4000 )
+		self.h02 = TH2F( 'h_02', 'h_02', 100, 0, 4000, 100, 0, 4000 )
+		self.h03 = TH2F( 'h_03', 'h_03', 100, 0, 4000, 100, 0, 4000 )
+		self.h12 = TH2F( 'h_12', 'h_12', 100, 0, 4000, 100, 0, 4000 )
+		self.h13 = TH2F( 'h_13', 'h_13', 100, 0, 4000, 100, 0, 4000 )
+		self.h23 = TH2F( 'h_23', 'h_23', 100, 0, 4000, 100, 0, 4000 )
 		
 		
 		
@@ -42,22 +42,17 @@ class ViewMU:
 		try:
 			m7 = qdc.moduls[7]
 			for a,e in m7:
-				if e == 71:
+				if e == 64:
 					mu[0] = a
+				if e == 65:
+					mu[2] = a
+				if e == 66:
+					mu[3] = a
+				if e == 71:
+					mu[1] = a
 		except:
 			pass
 
-		try:
-			m8 = qdc.moduls[8]
-			for a,e in m8:
-				if e == 23:
-					mu[1] = a
-				if e == 40:
-					mu[2] = a
-				if e == 47:
-					mu[3] = a
-		except:
-			pass
 
 		flag = [False,False,False,False]
 
